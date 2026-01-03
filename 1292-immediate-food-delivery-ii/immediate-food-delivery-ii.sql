051765d4aff38b08e9d4c0aa066fea3e022a6ebc
Beimnet Tefera <asefabeimnet1@gmail.com>
2026-01-03 16:07:09 +0300

    Time: 1128 ms (16.88%) | Memory: 0B (100.00%) - LeetSync

---
+++ 1292-immediate-food-delivery-ii/immediate-food-delivery-ii.sql
@@ -0,0 +1,17 @@
+/* Write your T-SQL query statement below */
+SELECT
+    ROUND(
+        100 * SUM( 
+                    CASE
+                        WHEN order_date = customer_pref_delivery_date THEN 1
+                        ELSE 0
+                    END
+        ) / CAST(COUNT(*) AS FLOAT), 
+    2) AS immediate_percentage
+FROM(
+    SELECT
+        *,
+        ROW_NUMBER() OVER (PARTITION BY customer_id ORDER BY order_date)AS FLAG
+    FROM Delivery
+) AS temp_table
+WHERE FLAG = 1
