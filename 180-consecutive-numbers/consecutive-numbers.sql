f0d408eb9555a900bcf576edc22c7a8d972f256e
Beimnet Tefera <asefabeimnet1@gmail.com>
2026-01-10 20:31:56 +0300

    Time: 712 ms (8.63%) | Memory: 0B (100.00%) - LeetSync

---
+++ 180-consecutive-numbers/consecutive-numbers.sql
@@ -0,0 +1,10 @@
+# Write your MySQL query statement below
+SELECT 
+    L1.num AS ConsecutiveNums 
+FROM Logs L1, Logs L2, Logs L3
+WHERE 
+    L1.num = L2.num
+    AND L1.num = L3.num
+    AND L2.id = L1.id + 1
+    AND L3.id = L1.id + 2
+GROUP BY L1.num;
