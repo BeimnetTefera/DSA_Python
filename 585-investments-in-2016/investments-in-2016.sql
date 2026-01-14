1e74d8504185dfc24206093694f42d489e2f9aaf
Beimnet Tefera <asefabeimnet1@gmail.com>
2026-01-14 08:57:30 +0300

    Time: 536 ms (52.20%) | Memory: 0B (100.00%) - LeetSync

--- 585-investments-in-2016/investments-in-2016.sql
+++ 585-investments-in-2016/investments-in-2016.sql
@@ -7,18 +7,8 @@ FROM
 (
     SELECT 
         *,
-        COUNT(*) OVER (PARTITION BY tiv_2015)AS total_people
-    FROM Insurance
-) AS table_1
-
-JOIN 
-
-(
-    SELECT
-        *,
+        COUNT(*) OVER (PARTITION BY tiv_2015)AS total_people,
         COUNT(tiv_2016) OVER(PARTITION BY lat, lon)AS total
     FROM Insurance
-) AS table_2
-
-ON table_1.pid = table_2.pid
+) AS table_1
 WHERE total = 1 AND total_people > 1
